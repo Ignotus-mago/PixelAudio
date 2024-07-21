a5d152069424362cf116170b41e2a79eef75765f
Paul Hertz <ignotus@gmail.com>
2024-07-21 16:12:14 -0500

    Create TestPixelAudio.java

---
+++ src/net/paulhertz/pixelaudio/TestPixelAudio.java
@@ -0,0 +1,25 @@
+package net.paulhertz.pixelaudio;
+
+import processing.core.PApplet;
+
+public class TestPixelAudio extends PApplet {
+
+	public void settings() {
+		size(1024, 1024);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+
+	public static void main(String args[]) {
+		PApplet.main(new String[] { "--present", "/Users/paulhz/Code/Workspace/PixelAudio/bin/net/paulhertz/pixelaudio/TestPixelAudio" });
+	}
+
+}
+
+
