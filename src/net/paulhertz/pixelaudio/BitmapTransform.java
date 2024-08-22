1ccd62066fc4f189d192028a09bda1007728229f
Paul Hertz <ignotus@gmail.com>
2024-08-22 12:21:04 -0500

    Update BitmapTransform.java

--- src/net/paulhertz/pixelaudio/BitmapTransform.java
+++ src/net/paulhertz/pixelaudio/BitmapTransform.java
@@ -11,7 +11,7 @@ import processing.core.PConstants;
  * the arrays may be bitmap pixel arrays, but not  necessarily--they could represent 
  * index values over a large bitmap or integer data of any sort, such as the LUTs generated 
  * by PixelMapGen subclasses. Methods that take a PImage as an argument will use the 
- * <code>PImage.pixels</code> array, and resize the image when necessary. *Map methods
+ * <code>PImage.pixels</code> array, and resize the image when necessary. Map methods
  * that accept width and height parameters return arrays of transformed pixel indices.</p>
  * 
  * @see AffineTransformType
