c82ef3192624c4fc8d024c6077b365710a3a7032
Paul Hertz <ignotus@gmail.com>
2025-10-19 18:12:27 -0500

    Update package-info.java

--- src/net/paulhertz/pixelaudio/curves/package-info.java
+++ src/net/paulhertz/pixelaudio/curves/package-info.java
@@ -6,6 +6,16 @@ for Bezier curve modeling.
  IgnoCodeLib is available in the Processing contributed libraries and at
  https://paulhertz.net/ignocodelib/ and https://github.com/Ignotus-mago/IgnoCodeLib3
 </p>
+<p>
+<ul>
+<li>PAVertex2DINF.java provides an interface for line and curve vertices.</li>
+<li>PALineVertex.java stores a line vertex Interface for line and curve vertices</li>
+<li>PABezVertex.java provides storage for a cubic Bezier curves's control points and anchor point.</li>
+<li>PABezShape.java stores open or closed paths composed of lines and Bezier curves, and implements attributes and geometric transforms for them.</li>
+<li>PACurveMaker.java is a utility and storage class for interactive drawing and curve modeling.</li>
+<li>PACurveUtility.java provides static methods that support curve modeling and drawing.</li>
+</ul>
+</p>
 
 
 */
