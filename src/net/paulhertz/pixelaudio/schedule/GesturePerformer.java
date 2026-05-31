0104cffff2df75120979b292ee357c4ae5308f2b
Paul Hertz <ignotus@gmail.com>
2026-05-30 19:09:30 -0500

    Update GesturePerformer.java

--- src/net/paulhertz/pixelaudio/schedule/GesturePerformer.java
+++ src/net/paulhertz/pixelaudio/schedule/GesturePerformer.java
@@ -3,9 +3,11 @@ package net.paulhertz.pixelaudio.schedule;
 import net.paulhertz.pixelaudio.curves.PACurveMaker;
 import net.paulhertz.pixelaudio.granular.GestureGranularConfig;
 
-// future GranularPerformer wraps GestureGranularRenderer
+// future GranularPerformer wraps granular gesture parameters
 // future SamplerPerformer wraps your TimedLocation-based playback
+// TODO delete, unused ... but maybe the concept is valuable?
 
+@Deprecated
 interface GesturePerformer {
 		
     void play(PACurveMaker curve, GestureGranularConfig config);
