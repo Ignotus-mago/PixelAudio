05f98db66fb0fa1b959f899286ddd356f117045c
Paul Hertz <ignotus@gmail.com>
2026-07-18 13:17:20 -0500

    Renamed directories in examples_data

--- src/net/paulhertz/pixelaudio/example/package-info.java
+++ src/net/paulhertz/pixelaudio/example/package-info.java
@@ -135,5 +135,23 @@
  * <p>Most examples extend Processing's PApplet and follow the standard setup/draw pattern.
  * Many support interactive keyboard control for real-time parameter manipulation.
  * The tutorial series (TutorialOne_*) provides progressive lessons in increasing complexity.</p>
+ * 
+ * <p><b>Examples Data Files</b></p>
+ * <p>
+ * The "examples_data" folder contains various data files for use with PixelAudio example sketches. 
+ * Some of the subfolders contain files that are intended for special purposes.
+ * </p>
+ * <ul>
+ *   <li>The JSON folder contains .json files intended for use with the WaveSynth examples.</li>
+ *   <li>The Bag, Body, and Sessions-Bagatelle folders are intended for use with Bagatelle presets, 
+ *       but also contain audio and image files you can load in most sketches.</li> 
+ *   <li>Bag contains a folder of brushes that you can load in Bagatelle using then 'o' key command.</li>
+ *   <li>Body contains a folder, session_02, that contains brush and audio configuration data 
+ *       that you can load in Bagatelle using then 'o' key command.</li>
+ *   <li>Sesssion-Bagatelle contains brush and audio configuration data 
+ *       that you can load in Bagatelle using then 'o' key command. </li> 
+ *   <li>The image and audio files can be opened by most of the example sketches.</li> 
+ * </ul>
+
  */
 package net.paulhertz.pixelaudio.example;
